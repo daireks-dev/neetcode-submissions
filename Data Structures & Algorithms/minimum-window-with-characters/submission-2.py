df5f260be89709b5640ba52d8eb64b0a5c16745e
daireks <81038636+daireks-dev@users.noreply.github.com>
2026-07-08 13:51:22 -0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        windowCount, stringCount = {}, {}
+        for char in t:
+            stringCount[char] = stringCount.get(char, 0) + 1
+
+        have, need = 0, len(stringCount)
+
+        #Useful because it accounts for the edge case of no valid answer
+        foundSubstring, foundLength = [-1, -1], float("infinity")
+
+        L = 0
+        for R in range(0, len(s)):
+            char = s[R]
+            windowCount[char] = windowCount.get(char, 0) + 1
+
+            #Whenever a unique character from t is added to the window:
+            if char in stringCount and windowCount[char] == stringCount[char]:
+                have += 1
+            
+            #How far can we reduce this substring until its invalid?
+            while have == need:
+                if (R - L + 1) < foundLength:
+                    foundSubstring = [L, R]
+                    foundLength = R - L + 1
+                windowCount[s[L]] -= 1
+                if s[L] in stringCount and windowCount[s[L]] < stringCount[s[L]]:
+                    have -= 1
+                L += 1
+
+        L, R = foundSubstring
+        return s[L: R + 1] if foundLength != float("infinity") else ""
+
