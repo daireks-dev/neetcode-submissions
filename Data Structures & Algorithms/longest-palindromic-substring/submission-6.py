868f973a20c06144ad96195a7515d0cd9d8207c6
daireks <81038636+daireks-dev@users.noreply.github.com>
2026-08-05 16:57:15 -0400

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+
+        longest = (0,0)
+        def search(L, R):
+            nonlocal longest
+
+            while L >= 0 and R < len(s):
+                if s[L] == s[R]:
+                    if longest[1] - longest[0] < R-L:
+                        longest = (L, R)
+                else:
+                    break
+                
+                L -= 1
+                R += 1
+
+        for i in range(0, len(s)):
+            #Odd palindromes
+            search(i, i)
+
+            #Even palindromes
+            search(i, i + 1)
+
+        return s[longest[0]:longest[1]+1]
+
+
+
